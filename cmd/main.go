6fbbe04c0005e6a57b66b24798fa5e50d41e6007
Baris Simsek <simsek@thincube.co>
2019-12-23 15:03:27 -0800

    added code comments

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/barissimsek/npp/scale"
 )
 
+// Usage: cmd -scale major -note C
 func main() {
 	var note = flag.String("note", "C", "Root note for the scale")
 	var s = flag.String("scale", "major", "scale: major, minor")
--- scale/scale.go
+++ scale/scale.go
@@ -1,8 +1,8 @@
 package scale
 
 var chromatic = []string{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"}
-var maj = [8]int{0, 2, 2, 1, 2, 2, 2, 1}
-var min = [8]int{0, 2, 1, 2, 2, 1, 2, 2}
+var maj = [8]int{0, 2, 2, 1, 2, 2, 2, 1} // Major intervals starting from the root note
+var min = [8]int{0, 2, 1, 2, 2, 1, 2, 2} // Minor intervals starting from the root note
 
 func index(note string) int {
 	for i, v := range chromatic {
@@ -19,12 +19,14 @@ func Get(s string, note string) [7]string {
 	var scale = [8]int{}
 	var i = index(note)
 
+	// Choose the interval
 	if s == "major" {
 		scale = maj
 	} else if s == "minor" {
 		scale = min
 	}
 
+	// Generate the scale from intervals
 	for j := 0; j < 7; j++ {
 		i = (i + scale[j]) % 12
 		major[j] = chromatic[i]
